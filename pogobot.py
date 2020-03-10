99784668ade3ec50b12697ea25684076cf0ea2c7
Luciani, Timothy <tluciani@conversantmedia.com>
2020-03-09 23:01:57 -0500

    -- Added a kill flag to the bot

--- pogobot.py
+++ pogobot.py
@@ -1033,7 +1033,7 @@ async def editraidrole(message, role):
 
 @bot.command(aliases=["ks"],
              pass_context=True)
-async def killscheduler(ctx, message):
+async def killscheduler(ctx):
     def confirm(m):
         if m.author == ctx.message.author:
             return True
