69a102e96c617ae16d9b8f52f73bf912820a6eca
Luciani, Timothy <tluciani@conversantmedia.com>
2020-10-31 18:53:11 -0500

    forgot to revert the path

--- pogobot.py
+++ pogobot.py
@@ -1771,8 +1771,8 @@ def getEmoji(name):
 
 
 if __name__ == "__main__":
-    # path = '/var/opt/PoGo-Bot/'
-    path = '/Users/tluciani/WebstormProjects/PoGo-Bot/'
+    path = '/var/opt/PoGo-Bot/'
+    # path = '/Users/tluciani/WebstormProjects/PoGo-Bot/'
     cfg = configparser.ConfigParser()
     cfg.read(path+'config.ini')
 
